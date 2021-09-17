c8bb9a7551e9c3597d4d7d572b239fe4bd59cecc
AMRISH K.S <ksamrishsuresh@gmail.com>
2021-09-17 21:05:43 +0530

    commit after removing localhost

--- demo/settings.py
+++ demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +82,7 @@ DATABASES = {
         'NAME': 'test',
         'USER': 'postgres',
         'PASSWORD': 'Sapspg@6',
-        'HOST': 'localhost'
+        # 'HOST': 'localhost'
     }
 }
 
@@ -134,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
